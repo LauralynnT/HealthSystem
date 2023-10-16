5ff9e29372da47bdab3bbf9dc75714e3f1fbb617
Lauralynn Trainor <144450292+LauralynnT@users.noreply.github.com>
2023-10-16 10:35:25 -0300

    added debug

--- HealthSystem/Program.cs
+++ HealthSystem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,41 +9,206 @@ namespace HealthSystem
 {
     internal class Program
     {
-        static int health = 100;
-        static int lives = 3;
-        static int shield = 100;
-        static Random rnd = new Random();
-        static Random rhp = new Random();
+        static int health;
+        static int lives;
+        static int shield;
+        //static Random rnd = new Random();
+        //static Random rhp = new Random();
         static ConsoleColor currentForeground = ConsoleColor.White;
         static void Main(string[] args)
         {
             currentForeground = ConsoleColor.White;
             Console.Write("Press Any Key To Start...\n");
             Console.ReadKey(true);
-            while (lives!= 0)
-            {
-                HUD();
-                Console.ReadKey(true);
-                Heal();
-                Console.ReadKey(true);
-            }
-            Console.WriteLine("\nOut of Lives");
-            ColorString(0, "Game Over");
+            UnitTestHealthSystem();
+            /*   while (lives!= 0)
+               {
+                   HUD();
+                   Console.ReadKey(true);
+                   Heal();
+                   Console.ReadKey(true);
+               }
+               Console.WriteLine("\nOut of Lives");
+               ColorString(0, "Game Over"); */
         }
-        static void Damage()
+        //guiygyugbugygygu
+        static void UnitTestHealthSystem()
         {
-           int rDmg = rnd.Next(1, 100);
+            Debug.WriteLine("Unit testing Health System started...");
+
+            // TakeDamage()
+
+            // TakeDamage() - only shield
+            shield = 100;
+            health = 100;
+            lives = 3;
+            Damage(10);
+            //TakeDamage(10);
+            Debug.Assert(shield == 90);
+            Debug.Assert(health == 100);
+            Debug.Assert(lives == 3);
+            HUD();
+
+            // TakeDamage() - shield and health
+            shield = 10;
+            health = 100;
+            lives = 3;
+            Damage(50);
+            Debug.Assert(shield == 0);
+            Debug.Assert(health == 60);
+            Debug.Assert(lives == 3);
+            HUD();
+
+            // TakeDamage() - only health
+            shield = 0;
+            health = 50;
+            lives = 3;
+            Damage(10);
+            Debug.Assert(shield == 0);
+            Debug.Assert(health == 40);
+            Debug.Assert(lives == 3);
+            HUD();
+
+            // TakeDamage() - health and lives
+            shield = 0;
+            health = 10;
+            lives = 3;
+            Damage(25);
+            Debug.Assert(shield == 0);
+            Debug.Assert(health == 0);
+            Debug.Assert(lives == 3);
+            HUD();
+
+            // TakeDamage() - shield, health, and lives
+            shield = 5;
+            health = 100;
+            lives = 3;
+            Damage(110);
+            Debug.Assert(shield == 0);
+            Debug.Assert(health == 0);
+            Debug.Assert(lives == 3);
+            HUD();
+
+            // TakeDamage() - negative input
+            shield = 50;
+            health = 50;
+            lives = 3;
+            Damage(-10);
+            Debug.Assert(shield == 50);
+            Debug.Assert(health == 50);
+            Debug.Assert(lives == 3);
+            HUD();
+
+            // Heal()
+
+            // Heal() - normal
+            shield = 0;
+            health = 90;
+            lives = 3;
+            Heal(5);
+            Debug.Assert(shield == 0);
+            Debug.Assert(health == 95);
+            Debug.Assert(lives == 3);
+            HUD();
+
+            // Heal() - already max health
+            shield = 90;
+            health = 100;
+            lives = 3;
+            Heal(5);
+            Debug.Assert(shield == 90);
+            Debug.Assert(health == 100);
+            Debug.Assert(lives == 3);
+            HUD();
+
+            // Heal() - negative input
+            shield = 50;
+            health = 50;
+            lives = 3;
+            Heal(-10);
+            Debug.Assert(shield == 50);
+            Debug.Assert(health == 50);
+            Debug.Assert(lives == 3);
+            HUD();
+
+            // RegenerateShield()
+
+            // RegenerateShield() - normal
+            shield = 50;
+            health = 100;
+            lives = 3;
+            RegenerateShield(10);
+            Debug.Assert(shield == 60);
+            Debug.Assert(health == 100);
+            Debug.Assert(lives == 3);
+            HUD();
+
+            // RegenerateShield() - already max shield
+            shield = 100;
+            health = 100;
+            lives = 3;
+            RegenerateShield(10);
+            Debug.Assert(shield == 100);
+            Debug.Assert(health == 100);
+            Debug.Assert(lives == 3);
+            HUD();
+
+            // RegenerateShield() - negative input
+            shield = 50;
+            health = 50;
+            lives = 3;
+            RegenerateShield(-10);
+            Debug.Assert(shield == 50);
+            Debug.Assert(health == 50);
+            Debug.Assert(lives == 3);
+            HUD();
+
+            // Revive()
+
+            // Revive()
+            shield = 0;
+            health = 0;
+            lives = 2;
+            //Revive();
+            Debug.Assert(shield == 100);
+            Debug.Assert(health == 100);
+            Debug.Assert(lives == 1);
+            HUD();
+
+            Debug.WriteLine("Unit testing Health System completed.");
+            Console.Clear();
+        }
+        static void RegenerateShield(int shieldHeal)
+        {
+            shield += shieldHeal;
+        }
+        static void Damage(int rDmg)
+        {
+            //int rDmg = rnd.Next(1, 100);
             Console.Write("\nPlayer takes ");
             ColorControl(1, rDmg);
             Console.Write(" damage\n");
             if (shield <= 0)
             {
                 health -= rDmg;
+                if (health <= 0)
+                {
+                    health = 0;
+                }
             }
-            
             else
             {
                 shield -= rDmg;
+                if (shield <= 0)
+                {
+                    health += shield;
+                    shield = 0;
+                    if (health <= 0)
+                    {
+                        health = 0;
+                    }
+                }
+
             }
         }
         static void HUD()
@@ -52,12 +218,14 @@ namespace HealthSystem
             Console.Write("\nLives: ");
             ColorControl(2, lives);
             Console.Write("\n");
+            Console.ReadKey(true);
+            Console.Write("\n");
         }
-        static void Heal()
+        static void Heal(int rDmg)
         {
-            int rDmg = rnd.Next(10, 75);
-            int roll = rhp.Next(1, 5);
-            if(roll == 4)
+            //int rDmg = rnd.Next(10, 75);
+            //int roll = rhp.Next(1, 5);
+            //if(roll == 4)
             {
                 Console.Write("\nPlayer Heals ");
                 ColorControl(1, rDmg);
@@ -65,11 +233,6 @@ namespace HealthSystem
                 if (shield <= 0)
                 {
                     health += rDmg;
-                }
-                else if (shield <= 0 && health >= 100)
-                {
-                    shield = 0;
-                    shield += rDmg;
                 }
                 else
                 {
@@ -79,14 +242,14 @@ namespace HealthSystem
                 {
                     health = 100;
                 }
-                
+
             }
-            else
+            //else
             {
-                Damage();
+                //    Damage();
             }
-             
-            }
+
+        }
         static void ColorControl(int color, int sColor)
         {
             if (color == 0)
@@ -103,7 +266,7 @@ namespace HealthSystem
             }
             else if (color == 2)
             {
-                Console.ForegroundColor= ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.Write(sColor);
                 Console.ForegroundColor = ConsoleColor.White;
             }
@@ -184,7 +347,7 @@ namespace HealthSystem
             }
             else if (color == 11)
             {
-                Console.ForegroundColor= ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Gray;
                 Console.Write(fColor);
                 Console.ForegroundColor = ConsoleColor.White;
             }
@@ -204,6 +367,7 @@ namespace HealthSystem
                 lives--;
                 Console.Write("Press Any Key To Continue...\n");
                 Console.ReadKey(true);
+                Console.Write("\n");
                 if (lives != 0)
                 {
                     Console.Write("Shield: ");
@@ -298,4 +462,7 @@ namespace HealthSystem
             }
         }
     }
+
 }
+
+
